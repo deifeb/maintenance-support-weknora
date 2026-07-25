7217617e8461548040c8f6b55b218223918a5ce3
deifeb <ruiyuan327@gamil.com>
2026-07-25 17:54:20 +0800

    feat: implement internal jwt verifier

--- extensions/maintenance-api/app/security/__init__.py
+++ extensions/maintenance-api/app/security/__init__.py
@@ -1,3 +1,9 @@
 from app.security.actor import ActorContext, MaintenanceRole
+from app.security.internal_jwt import InternalTokenError, InternalTokenVerifier
 
-__all__ = ["ActorContext", "MaintenanceRole"]
+__all__ = [
+    "ActorContext",
+    "InternalTokenError",
+    "InternalTokenVerifier",
+    "MaintenanceRole",
+]
